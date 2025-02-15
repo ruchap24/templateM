ae83ff8eaa4c77d4cb584dc1954aa46862f2058f
ruchap24 <patilrucha991@gmail.com>
2025-02-15 13:28:10 +0530

    Project skeleton and asset setup

---
+++ components/custom/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '../ui/button'
+
+function Header() {
+  return (
+    <div className='flex justify-between items-center p-4 shadow-sm px-10'>
+        <Image src={'/logo.svg'} alt="Logo" width={100} height={100} />
+        <div>
+            <Button>get started</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ components/custom/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='px-10 md:px-28 lg:px-44  flex flex-col items-center mt-24'>
+        <h2 className='font-extrabold text-5xl text-center'>ai power email templates
+            <span className='text-primary'>Email Templates</span>
+        </h2>
+        <p className='text-center text-lg mt-4'>Create beautiful email templates with ai power.</p>
+    </div>
+  )
+}
+
+export default Hero
