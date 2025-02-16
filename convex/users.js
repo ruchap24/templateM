4d3d433c8de1e8e074e63973dd5199828c9c395f
ruchap24 <patilrucha991@gmail.com>
2025-02-16 10:43:10 +0530

    Add CreateUser mutation to handle user creation and retrieval

---
+++ convex/users.js
@@ -0,0 +1,27 @@
+import { mutation } from "./_generated/server";
+
+export const CreateUser=mutation({
+    args:{
+        name:v.string(),
+        email:v.string(),
+        picture:v.string()
+    },
+    handler:async(convexToJson,args)=>{
+        //if user already exist
+        const user=await ctx.db.query('users')
+        .filter((q)=>q.eq(q.field('email'),args.email))
+        .collect();
+
+        //if not then add new user
+        if(user?.length==0){
+            const result=await ctx.db.insert('users',{
+                name:args.name,
+                email:args.email,
+                picture:args.picture,
+                credits:3
+            })
+            return result;
+        }
+        return user[0];
+    }
+})
