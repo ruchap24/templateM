445c211c43544a1f8fefdc6fbec431800f48c0a8
ruchap24 <patilrucha991@gmail.com>
2025-02-17 17:07:51 +0530

    Add Editor component with EditorHeader

---
+++ app/(main)/editor/[templateId]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EditorHeader from '@/components/custom/EditorHeader'
+
+function Editor() {
+  return (
+    <div>
+        <EditorHeader/>
+    </div>
+  )
+}
+
+export default Editor
