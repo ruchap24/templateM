239fd359ed0e6097f84d956caa011ae3d11b2491
ruchap24 <patilrucha991@gmail.com>
2025-02-17 17:52:42 +0530

    Add Layout data structure and integrate it into ElementsSideBar for dynamic rendering

--- components/custom/ElementsSideBar.jsx
+++ components/custom/ElementsSideBar.jsx
@@ -1,8 +1,19 @@
+import Layout from '@/Data/Layout'
 import React from 'react'
 
 function ElementsSideBar() {
   return (
-    <div>ElementsSideBar</div>
+    <div className='p-5'>
+        <h2 className='text-lg font-bold'>Layouts</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+            {Layout.map((layout, index) => (
+                <div key={index} className='flex flex-col items-center justify-center border border-dashed rounded-xl p-3'>
+                    <layout.icon className=''/>
+                    <h2 className='text-sm'>{layout.label}</h2>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
