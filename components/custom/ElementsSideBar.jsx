2453fdbe683273fc93249fa07fc4a6e7d64f4fd5
ruchap24 <patilrucha991@gmail.com>
2025-02-17 18:07:40 +0530

    Enhance ElementsSideBar layout

--- components/custom/ElementsSideBar.jsx
+++ components/custom/ElementsSideBar.jsx
@@ -5,11 +5,11 @@ function ElementsSideBar() {
   return (
     <div className='p-5'>
         <h2 className='text-lg font-bold'>Layouts</h2>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-3'>
             {Layout.map((layout, index) => (
                 <div key={index} className='flex flex-col items-center justify-center border border-dashed rounded-xl p-3 group hover:shadow-md hover:border-primary cursor-pointer'>
-                    <layout.icon className=''/>
-                    <h2 className='text-sm'>{layout.label}</h2>
+                    <layout.icon className='p-2 h-9 w-9 group-hover:text-primary group-hover:bg-purple-100 rounded-full'/>
+                    <h2 className='text-sm group-hover:text-primary'>{layout.label}</h2>
                 </div>
             ))}
         </div>
