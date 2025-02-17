2b036566511c8520fe79d6012149a58b6a598d8f
ruchap24 <patilrucha991@gmail.com>
2025-02-17 16:23:32 +0530

    Refactor Dashboard      ,add EmailTemplateList component

--- app/(main)/dashboard/Page.jsx
+++ app/(main)/dashboard/Page.jsx
@@ -1,17 +1,21 @@
 "use client"
-import { useUserDetail } from '@/app/provider'
+import { UserDetail } from '@/app/provider'
+import EmailTemplateList from '@/components/custom/EmailTemplateList'
 import Header from '@/components/custom/Header'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function Dashboard() {
-    const { userDetail, setUserDetail } = useUserDetail();
+    const { userDetail, setUserDetail } = UserDetail();
   return (
     <div>
         <Header/>
-        <div className='px-10 md:px-28 lg:px-40 xl:px-56'>
-            <div>
-                <h2>Hello,{userDetail?.name}</h2>
+        <div className='px-10 md:px-28 lg:px-40 xl:px-56 mt-16'>
+            <div className='flex justify-between items-center'>
+                <h2 className='font-bold text-3xl'>Hello,{userDetail?.name}</h2>
+                <Button>+ Create New Email Template</Button>
             </div>
+            <EmailTemplateList/>
         </div>
     </div>
   )
---
+++ components/custom/EmailTemplateList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EmailTemplateList() {
+  return (
+    <div>
+        <h2 className='font-bold text-xl text-primary mt-6'>Work Place</h2>
+    </div>
+  )
+}
+
+export default EmailTemplateList
