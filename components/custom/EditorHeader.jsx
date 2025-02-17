fc4c828924814573dc5d61fed32293a07c20b407
ruchap24 <patilrucha991@gmail.com>
2025-02-17 17:26:11 +0530

    Add Canvas, ElementsSideBar, and Settings components; enhance EditorHeader with device buttons

--- components/custom/EditorHeader.jsx
+++ components/custom/EditorHeader.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '../ui/button'
-import { Code } from 'lucide-react'
+import { Code, Monitor, Smartphone } from 'lucide-react'
 
 function EditorHeader() {
   return (
-    <div className='p-4 shadow-sm px-10 flex justify-between items-center'>
+    <div className='p-4 shadow-sm flex justify-between items-center'>
         <Image src={'/logo.svg'} alt='logo' width={160} height={150}/>
         <div>
-            
+            <Button variant='ghost'><Monitor/>Desktop</Button>
+            <Button variant='ghost'><Smartphone/>Mobile</Button>
         </div>
         <div className='flex gap-3'>
-            <Button>
+            <Button variant='ghost' className='hover:text-white hover:bg-primary'>
                 <Code />
             </Button>
-            <Button varient='outline'>Send Test Email</Button>
+            <Button variant='outline'>Send Test Email</Button>
             <Button>Save Template</Button>
         </div>
     </div>
---
+++ components/custom/ElementsSideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ElementsSideBar() {
+  return (
+    <div>ElementsSideBar</div>
+  )
+}
+
+export default ElementsSideBar
