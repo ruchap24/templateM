b9c5fd193fbf825d55996bbe857f25633675b621
ruchap24 <patilrucha991@gmail.com>
2025-02-17 16:39:02 +0530

    Add email list functionality to EmailTemplateList and update Hero image source

--- components/custom/EmailTemplateList.jsx
+++ components/custom/EmailTemplateList.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
+import Image from 'next/image'
+import { Button } from '../ui/button';
 
 function EmailTemplateList() {
+    const [emailList, setEmailList] = useState([]);
   return (
     <div>
         <h2 className='font-bold text-xl text-primary mt-6'>Work Place</h2>
+        {emailList?.length==0 &&
+        <div className='flex justify-center mt-7 flex flex-col items-center mt-10'>
+            <Image src={'/email.png'} alt='email' height=
+            {250} width={250}/>
+            <Button className='mt-7'>+ Create New</Button>
+            </div>
+        }
     </div>
   )
 }
