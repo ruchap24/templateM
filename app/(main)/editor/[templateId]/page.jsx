90aa55efed5007e6a6c8c8e1a27afcec4d220bbb
ruchap24 <patilrucha991@gmail.com>
2025-02-17 17:52:55 +0530

    Integrate Canvas, ElementsSideBar, and Settings components into Editor layout

--- app/(main)/editor/[templateId]/page.jsx
+++ app/(main)/editor/[templateId]/page.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Canvas from '@/components/custom/Canvas'
+import Settings from '@/components/custom/Settings'
+import ElementsSideBar from '@/components/custom/ElementsSideBar'
 import EditorHeader from '@/components/custom/EditorHeader'
+
 
 function Editor() {
   return (
     <div>
         <EditorHeader/>
+        <div className='grid grid-cols-5'>
+            <ElementsSideBar/>
+            <div className='col-span-3 bg-gray-100'>
+                <Canvas/>
+            </div>
+            <Settings/>
+        </div>
     </div>
   )
 }
