e35fb43124718c63c09c4e3940dae10dbb25e072
ruchap24 <patilrucha991@gmail.com>
2025-02-17 17:57:06 +0530

    Enhance ElementsSideBar layout items with hover effects and improved styling

--- components/custom/ElementsSideBar.jsx
+++ components/custom/ElementsSideBar.jsx
@@ -7,7 +7,7 @@ function ElementsSideBar() {
         <h2 className='text-lg font-bold'>Layouts</h2>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
             {Layout.map((layout, index) => (
-                <div key={index} className='flex flex-col items-center justify-center border border-dashed rounded-xl p-3'>
+                <div key={index} className='flex flex-col items-center justify-center border border-dashed rounded-xl p-3 group hover:shadow-md hover:border-primary cursor-pointer'>
                     <layout.icon className=''/>
                     <h2 className='text-sm'>{layout.label}</h2>
                 </div>
