3478b5178d4e8aaa47cda168f92440eaf751e338
ruchap24 <patilrucha991@gmail.com>
2025-02-18 12:04:36 +0530

    Implement drag-and-drop functionality for email templates

--- components/custom/Canvas.jsx
+++ components/custom/Canvas.jsx
@@ -1,14 +1,40 @@
 "use client"
-import { useScreenSize } from '@/app/provider'
-import React from 'react'
+import { useDragElementLayout, useEmailTemplate, useScreenSize } from '@/app/provider';
+import Layout from '@/Data/Layout';
+import React, { useState } from 'react'
 
 function Canvas() {
   const { screenSize, setScreenSize }= useScreenSize();
+  const {dragElementLayout, setDragElementLayout}=useDragElementLayout();
+  const {emailTemplate, setEmailTemplate}=useEmailTemplate();
+  const[dragOver, setDragOver]=useState(false);
+
+  const onDragOver=(e)=>{
+    e.preventDefault();
+    setDragOver(true);
+    console.log('over...')
+  }
+
+  const onDropHandle=()=>{
+    setDragOver(false);
+    if(dragElementLayout?.dragLayout){
+      setEmailTemplate(prev=>[...prev,dragElementLayout?.dragLayout])
+    }
+  }
 
   return (
     <div className='mt-20 flex justify-center'>
-      <div className={`bg-white p-6 w-full ${screenSize=='desktop'?'max-w-2xl':'max-w-md'}`}>
-
+      <div className={`bg-white p-6 w-full ${screenSize=='desktop'?'max-w-2xl':'max-w-md'}
+      ${dragOver && 'bg-purple-100 p-4'}`}
+      onDragOver={onDragOver}
+      onDrop={()=>onDropHandle()}
+      >
+        {emailTemplate?.length>0? emailTemplate?.map((Layout,index)=>(
+          <div key={index}>
+            Column {index+1}
+          </div>
+        )):
+        <h2 className='p-4 text-center bg-gray-100 border border-dashed'>Add Layout Here</h2>}
       </div>
     </div>
   )
--- components/custom/ElementsSideBar.jsx
+++ components/custom/ElementsSideBar.jsx
@@ -1,15 +1,31 @@
+"use client"
 import Layout from '@/Data/Layout'
 import React from 'react'
 import ElementLayoutCard from './ElementLayoutCard'
 import ElementList from '@/Data/ElementList'
+import { useDragElementLayout } from '@/app/provider'
 
 function ElementsSideBar() {
+
+  const {dragElementLayout, setDragElementLayout}=useDragElementLayout();
+
+  const onDragLayoutStart=(layout)=>{
+    setDragElementLayout({
+      dragLayout:{
+        ...layout,
+        id:Date.now()
+      }
+    })
+  }
+
   return (
     <div className='p-5 h-screen shadow-sm'>
         <h2 className='text-lg font-bold'>Layouts</h2>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-3'>
             {Layout.map((layout, index) => (
-                <ElementLayoutCard key={index} layout={layout}/>  
+              <div key={index} draggable onDragStart={()=>onDragLayoutStart(layout)}>
+                <ElementLayoutCard layout={layout}/>  
+              </div>
             ))}
         </div>
 
