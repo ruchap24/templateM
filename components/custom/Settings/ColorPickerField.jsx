778ca8bd647a2504ee680e2bc842f9884773f4f9
ruchap24 <patilrucha991@gmail.com>
2025-03-11 09:20:15 +0530

    update ColorPickerField

--- components/custom/Settings/ColorPickerField.jsx
+++ components/custom/Settings/ColorPickerField.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ColorPickerField({label, value, onHandleStyleChange}) {
   return (
-    <div>
+    <div className='grid'>
         <label>{label}</label>
         <input type="color" value={value} onChange={(e)=>onHandleStyleChange(e.target.value)}/>
     </div>
