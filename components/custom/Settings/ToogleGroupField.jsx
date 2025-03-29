a62dbde232f8f8394d537762984e7ea29f9c5f38
ruchap24 <patilrucha991@gmail.com>
2025-03-29 15:42:07 +0530

    add ToogleGroupField component for style selection in settings

---
+++ components/custom/Settings/ToogleGroupField.jsx
@@ -0,0 +1,26 @@
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import React from "react";
+
+function ToogleGroupField(label, value, options, onHandleStyleChange) {
+  return (
+    <div>
+      <label>{label}</label>
+      <ToggleGroup
+        type="single"
+        defaultValue={value}
+        onValueChange={(v) => onHandleStyleChange(v)}
+      >
+        {options.map((option, index) => (
+          <ToggleGroupItem key={index} value={option?.value} className="w-full">
+            <option.icon/>
+          </ToggleGroupItem>
+        ))}
+        {/* <ToggleGroupItem value="a">A</ToggleGroupItem>
+        <ToggleGroupItem value="a">B</ToggleGroupItem>
+        <ToggleGroupItem value="a">C</ToggleGroupItem> */}
+      </ToggleGroup>
+    </div>
+  );
+}
+
+export default ToogleGroupField;
