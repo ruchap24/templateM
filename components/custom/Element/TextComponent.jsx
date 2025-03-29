9557c915863207f12501a1ac40afb91b34ce05ce
ruchap24 <patilrucha991@gmail.com>
2025-03-29 15:41:27 +0530

    update TextComponent to apply full width styling for improved layout consistency

--- components/custom/Element/TextComponent.jsx
+++ components/custom/Element/TextComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TextComponent({style, content}) {
   return (
-    <div>
+    <div className='w-full'>
         <h2 style={style}>{content}</h2>
     </div>
   )
