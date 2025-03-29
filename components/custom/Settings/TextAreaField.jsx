5df9a1f7080ddfaf632ad636eea3a152194079de
ruchap24 <patilrucha991@gmail.com>
2025-03-29 15:42:00 +0530

    add TextAreaField component to enhance input handling in settings

--- components/custom/Settings/TextAreaField.jsx
+++ components/custom/Settings/TextAreaField.jsx
@@ -1,9 +1,11 @@
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 function TextAreaField({label,value,onHandleInputChange}) {
   return (
     <div>
         <label>{label}</label>
+        <Textarea value={value} onChange={(e)=>onHandleInputChange(e.target.value)} />
     </div>
   )
 }
