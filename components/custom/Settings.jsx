9467d53407824efcc3796bb0b9a79747ad234ba4
ruchap24 <patilrucha991@gmail.com>
2025-03-29 15:41:38 +0530

    add Settings component fields for text area, text alignment, and text transformation options

--- components/custom/Settings.jsx
+++ components/custom/Settings.jsx
@@ -5,6 +5,40 @@ import InputField from "./Settings/InputField";
 import ColorPickerField from "./Settings/ColorPickerField";
 import InputStyleField from "./Settings/InputStyleField";
 import SliderField from "./Settings/SliderField";
+import TextAreaField from "./Settings/TextAreaField";
+import ToogleGroupField from "./Settings/ToogleGroupField";
+import { AArrowUp, CaseLower, CaseUpper } from "lucide-react";
+import DropdownField from "./Settings/DropdownField";
+
+const TextAlignOptions = [
+  {
+    value: "left",
+    icon: AlignLeft,
+  },
+  {
+    value: "center",
+    icon: AlignLeft,
+  },
+  {
+    value: "right",
+    icon: AlignLeft,
+  },
+];
+
+const TextTransformOptions = [
+  {
+    value: "uppercase",
+    icon: CaseUpper,
+  },
+  {
+    value: "lowercase",
+    icon: CaseLower,
+  },
+  {
+    value: "capitalize",
+    icon: AArrowUp,
+  },
+];
 
 function Settings() {
   const { selectedElement, setSelectedElement } = useSelectedElement();
@@ -53,6 +87,15 @@ function Settings() {
           onHandleInputChange={(value) => onHandleInputChange("content", value)}
         />
       )}
+      {element?.textarea && (
+        <TextAreaField
+          label={"Text Area"}
+          value={element?.textarea}
+          onHandleInputChange={(value) =>
+            onHandleInputChange("textarea", value)
+          }
+        />
+      )}
       {element?.url && (
         <InputField
           label={"url"}
@@ -60,12 +103,24 @@ function Settings() {
           onHandleInputChange={(value) => onHandleInputChange("url", value)}
         />
       )}
-      {element?.style?.width &&
-      <SliderField label={'Width'} 
-          value={element?.style?.width} 
+      {element?.style?.width && (
+        <SliderField
+          label={"Width"}
+          value={element?.style?.width}
           type="%"
-          onHandleStyleChange={(value)=>onHandleInputChange('width',value)} />
-      }
+          onHandleStyleChange={(value) => onHandleInputChange("width", value)}
+        />
+      )}
+      {element?.style.textAlign && (
+        <ToogleGroupField
+          label={"Text Align"}
+          value={element?.style.textAlign}
+          options={TextAlignOptions}
+          onHandleStyleChange={(value) =>
+            onHandleStyleChange("textAlign", value)
+          }
+        />
+      )}
       {element?.style?.backgroundColor && (
         <ColorPickerField
           label="Background Color"
@@ -79,25 +134,65 @@ function Settings() {
         <ColorPickerField
           label="Text Color"
           value={element?.style?.color}
+          onHandleStyleChange={(value) => onHandleStyleChange("color", value)}
+        />
+      )}
+      {element?.style?.fontSize && (
+        <InputStyleField
+          label={"Font size"}
+          value={element?.style?.fontSize}
           onHandleStyleChange={(value) =>
-            onHandleStyleChange("color", value)
+            onHandleInputChange("fontSize", value)
           }
         />
       )}
-      {element?.style?.fontSize &&
-      <InputStyleField label={'Font size'} value={element?.style?.fontSize} onHandleStyleChange={(value)=>onHandleInputChange('fontSize',value)} />
-      }
-      {element?.style?.padding &&
-      <InputStyleField label={'Padding'} 
-          value={element?.style?.padding} 
-          onHandleStyleChange={(value)=>onHandleInputChange('padding',value)} />
-      }
-      {element?.style?.borderRadius &&
-      <SliderField label={'Border Radius'} 
-          value={element?.style?.borderRadius} 
-          onHandleStyleChange={(value)=>onHandleInputChange('borderRadius',value)} />
-      }
-      
+      {element?.style.textTransform && (
+        <ToogleGroupField
+          label={"Text Transform"}
+          value={element?.style.textTransform}
+          options={TextTransformOptions}
+          onHandleStyleChange={(value) =>
+            onHandleStyleChange("textTransform", value)
+          }
+        />
+      )}
+      {element?.style?.padding && (
+        <InputStyleField
+          label={"Padding"}
+          value={element?.style?.padding}
+          onHandleStyleChange={(value) => onHandleInputChange("padding", value)}
+        />
+      )}
+      {element?.style?.borderRadius && (
+        <SliderField
+          label={"Border Radius"}
+          value={element?.style?.borderRadius}
+          onHandleStyleChange={(value) =>
+            onHandleInputChange("borderRadius", value)
+          }
+        />
+      )}
+      {element?.style?.fontWeight && (
+        <DropdownField
+          label={"Font Weight"}
+          value={element?.style?.fontWeight}
+          options={['normal','bold']}
+          onHandleStyleChange={(value) =>
+            onHandleInputChange("fontWeight", value)
+          }
+        />
+      )}
+
+      {/* {element?.style.textAlign && (
+        <ToogleGroupField
+          label={"Text Align"}
+          value={element?.style.textAlign}
+          options={TextAlignOptions}
+          onHandleStyleChange={(value) =>
+            onHandleStyleChange("textAlign", value)
+          }
+        />
+      )} */}
     </div>
   );
 }
