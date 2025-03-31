7eba113f484659a07129b44062f550c7c676bfe0
ruchap24 <patilrucha991@gmail.com>
2025-03-31 19:38:13 +0530

    create ViewHtmlDialog component and import dialog comp

---
+++ components/custom/ViewHtmlDialog.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+function ViewHtmlDialog() {
+  return (
+    // sample code of dialog header 4.07.18
+    <div></div> // comment out this div
+  )
+}
+
+export default ViewHtmlDialog
