4948e84e1b851ad458c1bf20787405a5e5ae5481
ruchap24 <patilrucha991@gmail.com>
2025-04-01 15:56:44 +0530

    add button component and update style

--- app/(main)/dashboard/Page.jsx
+++ app/(main)/dashboard/Page.jsx
@@ -9,7 +9,7 @@ function Dashboard() {
     const { userDetail, setUserDetail } = UserDetail();
   return (
     <div>
-        <Header/>
+        {/* <Header/> */}
         <div className='px-10 md:px-28 lg:px-40 xl:px-56 mt-16'>
             <div className='flex justify-between items-center'>
                 <h2 className='font-bold text-3xl'>Hello,{userDetail?.name}</h2>
