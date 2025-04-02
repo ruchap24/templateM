fdbb3a86ea17e10f09263bfa9d2b3adf0d5191e1
ruchap24 <patilrucha991@gmail.com>
2025-04-02 18:28:11 +0530

    add tabs component to CreateNew page

--- app/(main)/dashboard/create/page.jsx
+++ app/(main)/dashboard/create/page.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-// import tabs shadcn component
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function CreateNew() {
   return (
-    <div className='px-10 md:px-20 lg:px-44 xl:px-56 mt-20'>
-        <div>
-            <h2 className='font-bold text-3xl'>
-                CREATE NEW EMAIL TEMPLATE
-            </h2>
-            <p className='text-lg text-gray-400'> Effortlessly design and customize professional Ai-power email templates with ease</p>
-            {/* tabs component code */}
-        </div>
+    <div className="px-10 md:px-20 lg:px-44 xl:px-56 mt-20">
+      <div>
+        <h2 className="font-bold text-3xl">CREATE NEW EMAIL TEMPLATE</h2>
+        <p className="text-lg text-gray-400">
+          {" "}
+          Effortlessly design and customize professional Ai-power email
+          templates with ease
+        </p>
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="password">Password</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="password">Change your password here.</TabsContent>
+        </Tabs>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateNew
+export default CreateNew;
