14eda3eba6b5f5a0fe458d0f5106cc87a41be924
ruchap24 <patilrucha991@gmail.com>
2025-04-02 19:06:51 +0530

    enhance ViewHtmlDialog with copy and close functionality

--- components/custom/ViewHtmlDialog.jsx
+++ components/custom/ViewHtmlDialog.jsx
@@ -5,26 +5,33 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
+import { Copy, X } from "lucide-react";
 
 function ViewHtmlDialog({ openDialog, htmlCode, closeDialog }) {
   const CopyCode = () => {
     navigator.clipboard.writeText(htmlCode);
   };
+
   return (
     <Dialog open={openDialog} onOpenChange={closeDialog}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle asChild>
-            <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between">
+            <DialogTitle asChild>
               <h2>HTML Email Template</h2>
+            </DialogTitle>
+            <div className="flex gap-2">
               <Copy
                 className="p-2 bg-gray-100 rounded-full h-9 w-9 cursor-pointer"
                 onClick={CopyCode}
               />
+              {/* Close button */}
+              <button onClick={closeDialog} className="p-2">
+                <X className="h-9 w-9" />
+              </button>
             </div>
-          </DialogTitle>
+          </div>
           <DialogDescription asChild>
             <div className="max-h-[400px] overflow-auto bg-black text-white rounded-lg p-5">
               <pre className="whitespace-pre-wrap break-all">
