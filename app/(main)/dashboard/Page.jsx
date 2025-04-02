35771a229185f260710dc13a220fb74a4945194e
ruchap24 <patilrucha991@gmail.com>
2025-04-02 18:28:29 +0530

    update Dashboard component to use Link

--- app/(main)/dashboard/Page.jsx
+++ app/(main)/dashboard/Page.jsx
@@ -1,24 +1,29 @@
 "use client"
 import { UserDetail } from '@/app/provider'
 import EmailTemplateList from '@/components/custom/EmailTemplateList'
-import Header from '@/components/custom/Header'
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import Link from 'next/link'
 
 function Dashboard() {
-    const { userDetail, setUserDetail } = UserDetail();
+  const { userDetail, setUserDetail } = UserDetail();
+
   return (
     <div>
-        {/* <Header/> */}
-        <div className='px-10 md:px-28 lg:px-40 xl:px-56 mt-16'>
-            <div className='flex justify-between items-center'>
-                <h2 className='font-bold text-3xl'>Hello,{userDetail?.name}</h2>
-                <Button>+ Create New Email Template</Button>
-            </div>
-            <EmailTemplateList/>
+      {/* <Header/> */}
+      <div className="px-10 md:px-28 lg:px-40 xl:px-56 mt-16">
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold text-3xl">Hello, {userDetail?.name}</h2>
+          <Link href="/dashboard/create">
+            <Button>
+              + Create New Email Template
+            </Button>
+          </Link>
         </div>
+        <EmailTemplateList />
+      </div>
     </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
