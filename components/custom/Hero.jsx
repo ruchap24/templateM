f0df49697fee3c793ebc1371f8f88b9d66624a3c
ruchap24 <patilrucha991@gmail.com>
2025-04-06 15:34:42 +0530

    fix: correct casing in Hero component heading for consistency

--- components/custom/Hero.jsx
+++ components/custom/Hero.jsx
@@ -6,8 +6,8 @@ import SignInButton from './SignInButton'
 function Hero() {
   return (
     <div className='px-10 md:px-28 lg:px-44  flex flex-col items-center mt-24'>
-        <h2 className='font-extrabold text-5xl text-center'>ai power email templates
-            <span className='text-primary'>Email Templates</span>
+        <h2 className='font-extrabold text-5xl text-center'>Ai Power 
+            <span className='text-primary'> Email Templates</span>
         </h2>
         <p className='text-center text-lg mt-4'>Create beautiful email templates with ai power.</p>
         <div className='flex gap-5 mt-6'>
