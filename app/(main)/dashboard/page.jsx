b69d4bfd4b17d47a8add0c99b055a11a98253c6a
ruchap24 <patilrucha991@gmail.com>
2025-12-06 18:30:16 +0530

    upgrade next v

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -27,3 +27,4 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
--- components/custom/EmailTemplateList.jsx
+++ components/custom/EmailTemplateList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import Image from 'next/image'
 import { Button } from '../ui/button';
+import Link from 'next/link';
 
 function EmailTemplateList() {
     const [emailList, setEmailList] = useState([]);
@@ -12,7 +13,9 @@ function EmailTemplateList() {
         <div className='flex justify-center mt-7 flex-col items-center'>
             <Image src={'/email.png'} alt='email' height=
             {250} width={250}/>
+            <Link href="/dashboard/create">
             <Button className='mt-7'>+ Create New</Button>
+            </Link>
             </div>
         }
     </div>
